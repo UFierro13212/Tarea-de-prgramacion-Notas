5fc73a5b02150e3f238e2e3baad6a93800acda49
UFierro13212 <uriel.fierro@udla.edu.ec>
2026-04-07 13:55:01 -0500

    agregue ingreso de notas con validacion
    en esta parte hice que el programa vaya estudiante por estudiante y materia por materia pidiendo las calificaciones. tambien agregue una validacion para que no acepte notas menores a 0 ni mayores a 10

--- main.c
+++ main.c
@@ -13,4 +13,26 @@ int main() {
     int reprobados;
 
     
+    for (i = 0; i < 5; i++) {
+
+    printf("\nEstudiante %d\n", i + 1);
+
+    for (j = 0; j < 3; j++) {
+
+        printf("Materia %d: ", j + 1);
+        scanf("%f", &notas[i][j]);
+
+        while (notas[i][j] < 0) {
+            printf("Error, ingrese otra: ");
+            scanf("%f", &notas[i][j]);
+        }
+
+        while (notas[i][j] > 10) {
+            printf("Error, ingrese otra: ");
+            scanf("%f", &notas[i][j]);
+        }
+    }
+}
+
     
+  
