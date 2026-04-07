dd8b0b51c73f8a389bf238d9c95acd11245682c2
UFierro13212 <uriel.fierro@udla.edu.ec>
2026-04-07 13:46:13 -0500

    inicio del programa y declaracion de variables
    declare las variables y la matriz para guardar las notas

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int i, j;
+    float notas[5][3];
+
+    float suma;
+    float mayor;
+    float menor;
+
+    int aprobados;
+    int reprobados;
+
+    
+    
